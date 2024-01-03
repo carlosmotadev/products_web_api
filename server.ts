225e3bc2e0569f980ea431015df5e25e502dce1c
ccmfile <ccmfile@gmail.com>
2024-01-02 22:31:57 -0300

    added routes app

--- server.ts
+++ server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import routes from './routes';
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+routes(app);
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
